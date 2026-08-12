0b9bc37514149e24abb1a26bc474bb0821575b14
Trap Houze Records <traphouzerec@gmail.com>
2026-08-12 14:04:05 +0100

    Revert "feat: add content admin and CMS worker"
    This reverts commit 609ac0bb222a768411ef3eb8f10e92119ac3535e.

--- assets/js/admin-config.example.js
+++
@@ -1,2 +0,0 @@
-// Copie para admin-config.js e defina o URL público do Worker.
-// window.CMS_API_URL = 'https://trap-houze-cms.seu-utilizador.workers.dev';
--- assets/js/admin-config.js
+++
@@ -1,3 +0,0 @@
-// URL público do Worker. É seguro guardar esta configuração no repositório.
-// Atualize este valor depois de publicar o Worker.
-window.CMS_API_URL = '';
